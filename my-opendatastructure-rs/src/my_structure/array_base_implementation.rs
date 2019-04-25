4f75211b8faff68496f40d7aa4b9d658ac18b04f
Koji Ota <afterjnih@gmail.com>
2019-04-25 17:19:09 +0900

    array deque

--- my-opendatastructure-rs/src/my_structure/array_base_implementation.rs
+++ my-opendatastructure-rs/src/my_structure/array_base_implementation.rs
@@ -155,3 +155,88 @@ impl<T: Clone> Queue<T> for ArrayQueue<T> {
         Some(x)
     }
 }
+
+struct ArrayDeque<T> {
+    a: Vec<T>,
+    j: usize,
+    n: usize,
+}
+
+impl<T> ArrayDeque<T> {
+    fn resize(&mut self) {}
+}
+
+impl<T: Clone> List<T> for ArrayDeque<T> {
+    fn size(&self) -> usize {
+        self.n
+    }
+
+    fn get(&self, index: usize) -> Option<&T> {
+        if index >= self.size() {
+            return None;
+        }
+        Some(&self.a[(self.j + index) % self.a.capacity()])
+    }
+
+    fn set(&mut self, index: usize, x: T) -> Option<T> {
+        if index >= self.size() {
+            return None;
+        }
+        let mut x_slice = vec![x];
+        let logical_index = (self.j + index) % self.a.capacity();
+        self.a[logical_index..=logical_index].swap_with_slice(&mut x_slice[0..=0]);
+        x_slice.into_iter().last()
+    }
+    fn add(&mut self, index: usize, x: T) {
+        if (self.n + 1 >= self.a.capacity()) {
+            self.resize();
+        }
+        if (index < self.n / 2) {
+            self.j = if self.j == 0 {
+                self.a.capacity() - 1
+            } else {
+                self.j - 1
+            };
+            for k in 0..(self.j - 1) {
+                let physical_index = (self.j + k) % self.a.capacity();
+                let next_physical_index = (self.j + k + 1) % self.a.capacity();
+                self.a[physical_index] = self.a[next_physical_index].clone();
+            }
+        } else {
+            for k in ((index + 1)..=(self.n)).rev() {
+                let physical_index = (self.j + k) % self.a.capacity();
+                let prev_physical_index = (self.j + k - 1) % self.a.capacity();
+                self.a[physical_index] = self.a[prev_physical_index].clone();
+            }
+        }
+        let i = (self.j + index) % self.a.capacity();
+        self.a[i] = x;
+        self.n += 1;
+    }
+
+    fn remove(&mut self, index: usize) -> Option<T> {
+        if index >= self.size() {
+            return None;
+        }
+        let x = self.a[(self.j + index) % self.a.capacity()].clone();
+        if (index < self.n / 2) {
+            for k in (1..=index).rev() {
+                let physical_index = (self.j + k) % self.a.capacity();
+                let prev_physical_index = (self.j + k - 1) % self.a.capacity();
+                self.a[physical_index] = self.a[prev_physical_index].clone();
+            }
+            self.j = (self.j + 1) % self.a.capacity();
+        } else {
+            for k in index..(self.n - 1) {
+                let physical_index = (self.j + k) % self.a.capacity();
+                let next_physical_index = (self.j + k + 1) % self.a.capacity();
+                self.a[physical_index] = self.a[next_physical_index].clone();
+            }
+        }
+        self.n -= 1;
+        if 3 * self.n < self.a.capacity() {
+            self.resize()
+        }
+        Some(x)
+    }
+}
